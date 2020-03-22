349b5f2648c625b943d80b27e6d730520fe5d788
GagliardiEmiliano <emiliano.gagliardi@e-novia.it>
2020-03-22 19:53:03 +0100

    two examples of how variadic templates are used in std lib

--- src/cpp-templates-the-complete-guide/4-variadic-templates/main.cpp
+++ src/cpp-templates-the-complete-guide/4-variadic-templates/main.cpp
@@ -1,6 +1,25 @@
 #include "print.h"
 #include "fold.h"
 
+// An example of use of variadic template
+// (as for make_shared, make_unique....)
+template <typename T, typename... Args>
+T make(Args&&... args) // perfectly forward arguments
+{
+    return T(args...);
+}
+
+template <typename F, typename... Args>
+auto call_function(F&& f, Args&&... args)
+{
+    return f(args...);
+}
+
+int max(int a, int b)
+{
+    return b < a ? a : b;
+}
+
 int main()
 {
     // print
@@ -17,4 +36,10 @@ int main()
     tree->left->right = new Node(3);
     auto n = traverse(tree, &Node::left, &Node::right);
     std::cout << "\n" << (n == tree->left->right) << "\n";
+    // use make function
+    auto p = make<std::pair<int, int>>(1, 3);
+    std::cout <<"\ncreated pair with make " << p.first << " " << 
+        p.second << "\n";
+    std::cout << "\ncalled function max with call_function " << 
+        call_function(max, 2, 1) << "\n";
 }
