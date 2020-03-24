4167f07dbcc8fdea03ff478b04e8fa17b1ac5bb2
Emiliano Gagliardi <emilianogagliardi@icloud.com>
2020-03-24 21:33:26 +0100

    class templates examples

--- src/cpp-templates-the-complete-guide/4-variadic-templates/main.cpp
+++ src/cpp-templates-the-complete-guide/4-variadic-templates/main.cpp
@@ -10,9 +10,9 @@ int max(int a, int b)
     return b < a ? a : b;
 }
 
-int main()
+void use_print()
 {
-    // print
+    std::cout << "USE PRINT\n";
     print(1, 2, 1, "ciao", 1.2, "sasso");
     std::cout << "\n";
     print2('a', 1, 2.3, "b", true);
@@ -20,30 +20,69 @@ int main()
     printDoubled(1, 2, 3, std::string("ciao"));
     std::cout << "\n";
     printPlusOne(1, 2, 3, 4);
-    std::cout << "\n";
+    std::cout << "\n\n";
+}
+
+void use_variadic_expressions()
+{
+    std::cout << "USE VARIADIC EXPRESSIONS\n";
     // sum with fold
-    std::cout << sum_foldl(1, 2, 3, 2.1) << "\n";
+    std::cout << "sum_foldl(1, 2, 3, 2.1)=" << sum_foldl(1, 2, 3, 2.1) << "\n";
     // traverse tree with fold
     Node* tree = new Node(1);
     tree->left = new Node(2);
     tree->left->right = new Node(3);
     auto n = traverse(tree, &Node::left, &Node::right);
-    std::cout << "\n" << (n == tree->left->right) << "\n";
-    // use make function
-    auto p = make<std::pair<int, int>>(1, 3);
-    std::cout <<"\ncreated pair with make " << p.first << " " << 
-        p.second << "\n";
-    std::cout << "\ncalled function max with call_function " << 
-        call_function(max, 2, 1) << "\n";
+    std::cout << "correctly traversed: " << (n == tree->left->right) << "\n";
     // example compile time variadic expression
-    std::cout << "\nisHomogeneous(1, 1, 2, 3) = " << isHomogeneous(1, 1, 2, 3);
-    std::cout << "\nisHomogeneous('1', 1, 2, 2.1) = " << isHomogeneous('1', 1, 2, 2.1) << "\n";
+    std::cout << "isHomogeneous(1, 1, 2, 3) = " << isHomogeneous(1, 1, 2, 3) << "\n";
+    std::cout << "isHomogeneous('1', 1, 2, 2.1) = " << isHomogeneous('1', 1, 2, 2.1) << "\n";
     // example variadic indices
+    std::cout << "print with variadic indices\n";
     int a[] = { 1, 2, 3, 4, 5 };
     printElems(a, 2, 3, 1, 0);
     std::cout << "\n";
     printIdx<2, 3, 1, 0>(a);
-    std::cout << "\n";
+    std::cout << "\n\n";
+}
+
+void use_make_and_call()
+{
+    std::cout << "USE MAKE AND CALL\n";
+    auto p = make<std::pair<int, int>>(1, 3);
+    std::cout << "created pair with make " << p.first << " " <<
+        p.second << "\n";
+    std::cout << "called function max with call_function " <<
+        call_function(max, 2, 1) << "\n\n";
+}
+
+void use_variadic_classes()
+{
+    std::cout << "USE VARIADIC CLASSES\n";
+    // example of use of variadic template class
     std::array<int, 3> container{ 1, 2, 3 };
     printElements2(container, Indices<0, 1>{});
+    std::cout << "\n";
+   
+    // example of array with deduction guide
+    my_array<int, 3> my_arr{ 1, 2, 3 };
+    for (auto i : my_arr) std::cout << i << " ";
+    std::cout << "\n";
+    // the following gives compile error because of my_array constructor
+    // deduction guide
+    // my_array<int, 3> my_arr2{ 1, 2, 3 };
+
+    // example of hineritance from variadic template
+    // combine hasher and equality for customers in one type:
+    using CustomerOP = Overloader<CustomerHash, CustomerEq>;
+    std::unordered_set<Customer, CustomerHash, CustomerEq> coll1;
+    std::unordered_set<Customer, CustomerOP, CustomerOP> coll2;
+}
+
+int main()
+{
+    use_print();
+    use_variadic_expressions();
+    use_make_and_call();
+    use_variadic_classes();
 }
--- src/cpp-templates-the-complete-guide/4-variadic-templates/variadic_class_templates.h
+++ src/cpp-templates-the-complete-guide/4-variadic-templates/variadic_class_templates.h
@@ -1,6 +1,11 @@
 #ifndef VARIADIC_CLASS_TEMPLATES_H
 #define VARIADIC_CLASS_TEMPLATES_H
 
+#include <type_traits>
+#include <unordered_set>
+
+// Template type represented by some indices
+
 template<size_t... Idx>
 struct Indices {};
 
@@ -9,5 +14,47 @@ void printElements2(C c, Indices<Idx...>)
 {
     print(std::get<Idx>(c)...);
 }
+
+// Array style class
+
+template<typename T, size_t S> class my_array {
+public:
+    template<typename U, typename... Us>
+    my_array(U u, Us... us) : data{u, us...} {}
+    T* begin() { return data; }
+    T* end() { return data + S; }
+private:
+    T data[S];
+};
+
+// deduction guide for array constructor
+template<typename U, typename... Us> my_array(U, Us...)->my_array<
+    std::enable_if_t<(std::is_same_v<U, Us> && ...), U>,
+    (1 + sizeof...(Us))>;
+
+// example: deriving the variadic argument
+
+struct Customer {
+    std::string name;
+};
+
+struct CustomerEq {
+    bool operator() (const Customer& c1, const Customer& c2) const
+    {
+        return c1.name == c2.name;
+    }
+};
+
+struct CustomerHash {
+    std::size_t operator() (const Customer& c) const
+    {
+        return std::hash<std::string>()(c.name);
+    }
+};
+
+template<typename... Bases> 
+class Overloader : Bases... {
+    using Bases::operator()...;
+};
 
 #endif // !VARIADIC_CLASS_TEMPLATES_H
